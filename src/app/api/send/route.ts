3d0ed8511a071a004026b1f503a4406dddfd7cf1
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-10-14 14:13:03 -0300

    fix(api/accept-invitation, api/send): normalize indentation and remove extra blank line

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import ForgotPasswordEmail from "@/components/emails/TemplateForgotPasswordEmail";
 
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
